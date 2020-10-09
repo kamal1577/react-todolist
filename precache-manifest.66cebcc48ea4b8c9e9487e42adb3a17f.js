cd2c7cf6828b8382b12dbd429d2c5334c8243a4b
kamalelazizialaoui <elazizialaouikamal@yahoo.com>
2020-10-09 01:59:09 -0400

    Updates

---
+++ precache-manifest.66cebcc48ea4b8c9e9487e42adb3a17f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e74796fb9941a0ae6b62073be982b2b",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "8ab26896cc5dbf5580e0",
+    "url": "/react-todolist/static/css/main.05f5bdfd.chunk.css"
+  },
+  {
+    "revision": "4a8e5ab12b6eeec4a3fe",
+    "url": "/react-todolist/static/js/2.1d1ea1b1.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/react-todolist/static/js/2.1d1ea1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab26896cc5dbf5580e0",
+    "url": "/react-todolist/static/js/main.65f3acb1.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
